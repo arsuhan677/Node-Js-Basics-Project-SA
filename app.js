aa9b35916529b6eb4b50c348e53af46821ce127c
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-03 01:11:19 +0600

    update for delete api

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+// parse application/json
+app.use(bodyParser.json());
+
 const port = 3000;
+const data = require("./data.json");
+
+const fs = require("fs");
+const path = require("path");
+const dataPath = path.join(__dirname, "data.json");
 
 app.get("/", (req, res) => {
   res.send("Example to application is runnig ! Port: " + port);
@@ -27,7 +39,7 @@ app.get("/products", (req, res) => {
   }
 });
 
-app.get("/produsts/:id", (req, res) => {
+app.get("/products/:id", (req, res) => {
   try {
     const id = req.params.id;
     const findProduct = data.find((product) => product.id === parseInt(id));
@@ -46,27 +58,91 @@ app.post("/products", (req, res) => {
     const { name, price, description, image, category, url } = req.body;
 
     if (!name || !price) {
-      return res.status(400).json({ message: "product in not a your database" });
+      return res
+        .status(400)
+        .json({ message: "product in not a your database" });
     }
 
     console.log("req2", req.body);
-    const data = require('./data.json')
-    const newId = data.length > 0? Math.max(...data.map(p => p.id)) + 1 : 1 
+    const data = require("./data.json");
+    const newId = data.length > 0 ? Math.max(...data.map((p) => p.id)) + 1 : 1;
 
     const newData = {
-        id: newId,
-        name,
-        price,
-        description,
-        image,
-        category,
-        url,
+      id: newId,
+      name,
+      price,
+      description,
+      image,
+      category,
+      url,
+    };
+
+    data.push(newData);
+
+    fs.writeFileSync(dataPath, JSON.stringify(data));
+    console.log("product create succesfully!!!");
+    res.status(201).json({ message: "product is succesfully..." });
+  } catch (error) {
+    res.status(500).json({ message: error.massege });
+  }
+});
+
+// update
+
+app.put("/product/:id", (req, res) => {
+  try {
+    // const id = req.params.id;
+    const id = parseInt(req.params.id);
+    const findProduct = data.find((product) => product.id === parseInt(id));
+
+    if (!findProduct) {
+      return res.status(404).json({ message: "product is not found" });
     }
 
-    data.push(newData)
+    const { name, price, description, image, category, url } = req.body;
+    findProduct.name = name;
+    findProduct.price = price;
+    findProduct.image = image;
+    findProduct.description = description;
+    findProduct.category = category;
+    findProduct.url = url;
 
-  } catch (error) {}
+    const data = require("./data.json");
+
+    const index = data.findIndex((product) => product.id === parseInt(id));
+    data[index] = findProduct;
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+// delete
+
+app.delete("/product/:id", (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const index = data.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      return res.status(404).json({ message: "product is not found" });
+    }
+
+    const deletedProduct = data[index];
+
+    data.splice(index, 1);
+
+    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+
+    res.status(200).json({
+      message: "product deleted successfully",
+      product: deletedProduct,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Application runnig at ${port}`);
