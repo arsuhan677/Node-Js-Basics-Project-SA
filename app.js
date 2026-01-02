98e64d72cb03b815d860e9e18621d224c6506bf2
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-03 00:28:50 +0600

    update for posti

--- app.js
+++ app.js
@@ -19,23 +19,55 @@ app.get("/products", (req, res) => {
 
       console.log("filterbyname", filteredData);
       res.json(filteredData);
-      
     } else {
       res.json(data);
     }
-
   } catch (error) {
     res.status(500).json({ message: error.massege });
   }
-
 });
 
-app.post("product/:id", (req, res) => {
-  console.log("the quick brown jumps fox over the lazy dog")
-})
+app.get("/produsts/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const findProduct = data.find((product) => product.id === parseInt(id));
+
+    if (!findProduct) {
+      return res.status(404).json({ message: "product is not found" });
+    }
+    res.status(200).json(findProduct);
+  } catch (error) {
+    return res.status(500).json({ message: error.massege });
+  }
+});
+
+app.post("/products", (req, res) => {
+  try {
+    const { name, price, description, image, category, url } = req.body;
+
+    if (!name || !price) {
+      return res.status(400).json({ message: "product in not a your database" });
+    }
+
+    console.log("req2", req.body);
+    const data = require('./data.json')
+    const newId = data.length > 0? Math.max(...data.map(p => p.id)) + 1 : 1 
+
+    const newData = {
+        id: newId,
+        name,
+        price,
+        description,
+        image,
+        category,
+        url,
+    }
+
+    data.push(newData)
+
+  } catch (error) {}
+});
 
 app.listen(port, () => {
-  console.log(`Application runnig at ${port}`)
-})
-
-
+  console.log(`Application runnig at ${port}`);
+});
